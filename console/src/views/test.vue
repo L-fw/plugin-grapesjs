f167b477a7d6cd7437df4da87e5bcdf0f8fd4adf
fawen <1076697947@qq.com>
2023-05-11 09:58:52 +0800

    plugin-grapes

---
+++ console/src/views/test.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import {defineProps} from "vue";
+import GrapesComponent from "@/views/grapesJsComponent.vue";
+
+const props = defineProps({
+  raw: {
+    type: String,
+    required: false,
+    default: "",
+  },
+  content: {
+    type: String,
+    required: false,
+    default: "",
+  },
+});
+
+const emit = defineEmits<{
+  (event: "update:raw", value: string): void;
+  (event: "update:content", value: string): void;
+  (event: "update", value: string): void;
+}>();
+
+const handleChange = (v: string) => {
+  emit("update:raw", v);
+  emit("update:content", v);
+  console.log(props.raw);
+  if (v !== props.raw) {
+    emit("update", v);
+  }
+};
+
+</script>
+
+<template>
+  <div>
+    <grapes-component :value="raw" @change="handleChange">
+    </grapes-component>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
